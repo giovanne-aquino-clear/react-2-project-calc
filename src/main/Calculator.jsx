9b940ca2a3e7a12767972628034941bbb56d1f39
giovanne <giovanne.aquino.clear@gmail.com>
2023-09-13 11:41:38 -0300

    add logic operations and  finalize project

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -31,7 +31,24 @@ export default class Calculator extends Component {
         } else {
             const equals = operation === '='
             const currentOperation = this.state.operation
-             
+
+            const values = [...this.state.operation]
+            try{
+                values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
+            } catch(e){
+                values[0]=this,this.state.values[0]
+                
+            }
+            
+            values[1] =  0
+
+            this.setState({
+                displayValue:values[0],
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDisplay: !equals,
+                values
+            })
         }
 
     }
